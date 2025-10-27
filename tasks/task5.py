8f5621bbaf99dd761343263497750c288b97a621
Qwerty-qjr <qwerty569h@mail.ru>
2025-10-28 00:25:21 +0500

    reqwijfuiw

--- tasks/task5.py
+++ tasks/task5.py
@@ -3,7 +3,12 @@
 def solve():
 # Ниже пишите решение задачи
 
-
+    n = int(input())
+    min1 = n // 60
+    hour = min1 // 60
+    min = min1 % 60
+    sec = n % 60
+    print (hour % 24, min, sec)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
